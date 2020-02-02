bdea87dfc9fb4b6e6a69db47693682ebb9388b36
Eze32 <ezeq.quevedo@gmail.com>
2020-02-02 20:29:42 -0300

    Se agrega Usuario metadata.

---
+++ HomeWork/Datos/Metadata/UsuarioMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Datos
+{
+    public partial class UsuarioMetadata
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string RepetirEmail { get; set; }
+        public string numCelular { get; set; }
+        public string contrasenia { get; set; }
+        public string RepetirContrasenia { get; set; }
+    }
+}
